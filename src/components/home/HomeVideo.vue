853a55bd5dc11da374a74828786384eeaa6c3ffd
Piotr Szyma <thompson2908@gmail.com>
2018-06-17 14:39:46 +0200

    iphone support for autoplay

--- src/components/home/HomeVideo.vue
+++ src/components/home/HomeVideo.vue
@@ -1,5 +1,5 @@
 <template>
-  <video playsinline autoplay muted loop>
+  <video playsinline webkit-playsinline webkitplaysinline autoplay muted loop>
     <source src="@/assets/video/vid.webm" type="video/webm">
     <source src="@/assets/video/vid.mp4" type="video/mp4">
     <img src="@/assets/video/vid.png" alt="Background video">
